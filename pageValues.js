f37bac08920c2f460158daceaf52ee8bc2baa5b7
Angad Trehan <angadtrehan@HDT022200234.local>
2025-04-07 01:18:17 +0530

    Implement all day pages

---
+++ pageValues.js
@@ -0,0 +1,55 @@
+export function getPageValues(look) {
+    switch (look) {
+        case 'blue':
+            return {
+                headerImageSource: '../assets/Day 1 - Blue Kurta Set.png',
+                headerTitle: 'Baby Blues',
+                pairedWithImageSource: '../assets/White Shoes.png',
+                descriptionText: "You look gorgeous baby, this was such a good find. It's so elegant and pairing it with the white shoes AND your braids... *chef's kiss*. You look amazing tinki, such a good outfit hehehe I love it so much.",
+                headerColour: 'rgb(199, 216, 250)',
+                pairedWithBgColour: 'rgb(220, 228, 245)',
+                headerTitleColour: 'rgb(0, 20, 0)'
+            }
+        case 'black':
+            return {
+                headerImageSource: '../assets/Day 2 - Black Dress Set.png',
+                headerTitle: 'Black on Black',
+                pairedWithImageSource: '../assets/White Shoes.png',
+                descriptionText: "I absolutely LOVE this one - it's my second favorite. You look absolutely stunning my jaan, this whole outfit is top class HEHE. I wish I was with you right now and could watch you do one fashion walk :')",
+                headerColour: 'rgb(16, 15, 20)',
+                pairedWithBgColour: 'rgb(62, 57, 81)',
+                headerTitleColour: 'rgb(155, 152, 152)'
+            }
+        case 'rust':
+            return {
+                headerImageSource: '../assets/Day 3 - Rust.png',
+                headerTitle: 'Rustic Brillance',
+                pairedWithImageSource: '../assets/White Shoes.png',
+                descriptionText: "This. This is my favorite. I actually found out while looking for the images that this is supposed to be a pyjama set. Trust my ninki's eye to see what others cannot hehe. You look absolutely fucking beautiful. I love it so much",
+                headerColour: 'rgb(113, 30, 31)',
+                pairedWithBgColour: 'rgb(126, 61, 51)',
+                headerTitleColour: 'rgb(20, 0, 0)'
+            }
+        case 'dungaree':
+            return {
+                headerImageSource: '../assets/Day 4 - Dungaree.png',
+                headerTitle: 'Dungaree Doo',
+                pairedWithImageSource: '../assets/White Shoes.png',
+                descriptionText: "You're making me want to put you in between two peices of bread and just FKN GOBBLE YOU UP. I genuinely cannot get enough of you in this, you're looking beautiful baby. I love it i love it HEHE",
+                headerColour: 'rgb(50, 77, 116)',
+                pairedWithBgColour: 'rgb(93, 130, 211)',
+                headerTitleColour: 'rgb(0, 3, 20)'
+            }
+        case 'beige':
+            return {
+                headerImageSource: '../assets/Day 5 - Beige Dress.png',
+                headerTitle: 'Sunflower Power',
+                pairedWithImageSource: '../assets/White Shoes.png',
+                descriptionText: "Last, but by no means the least - elegant, timeless, effortlessly pretty. You look absolutely perfect. I'm the luckiest idiot in the entire universe to have you be mine, nayaneeka. Ab bas jaldi se mere paas aaja 😭",
+                headerColour: 'rgb(189, 179, 163)',
+                pairedWithBgColour: 'rgb(169, 151, 121)',
+                headerTitleColour: 'rgb(24, 15, 0)',
+                isLast: true
+            }
+    }
+}
--- script.js
+++ script.js
@@ -1,24 +1,9 @@
-gsap.registerPlugin(ScrollTrigger);
-
-// gsap animation
-let tl = gsap.timeline({
-    scrollTrigger: {
-        trigger: '.intro',
-        start: 'top top',
-        end: 'bottom center',
-        scrub: true,
-        markers: false,
-        pin: true,
-        pinSpacing: false
-    }
-});
-
-tl.to('.intro', {
-    y: -1000
-});
+import { getPageValues }from './pageValues.js';
 
 // Initialize Lenis
-const lenis = new Lenis();
+const lenis = new Lenis({
+    syncTouch: true
+});
 
 // Use requestAnimationFrame to continuously update the scroll
 function raf(time) {
@@ -27,3 +12,132 @@ function raf(time) {
 }
 
 requestAnimationFrame(raf);
+
+const body = document.querySelector('.view');
+
+if (new Date().setHours(0,0,0,0) < new Date('04/07/2025').setHours(0,0,0,0)) {
+    console.log('site should not be accessed from index');
+    const message = document.createElement('div');
+    const title = document.createElement('h1');
+    const sub = document.createElement('p');
+    const img = document.createElement('div');
+    img.innerHTML = '<img src="./assets/Screenshot_2025-04-06_at_18.10.12-removebg-preview.png" alt="">';
+    img.classList.add('icon');
+    sub.appendChild(document.createTextNode('The full site can be accessed after you get back, come home fastly'));
+    sub.id = 'by-ninki';
+    title.appendChild(document.createTextNode('NOT YET'));
+    message.classList.add('intro');
+    message.appendChild(title);
+    message.appendChild(sub);
+    message.appendChild(img);
+    body.innerHTML = '';
+    body.appendChild(message);
+    body.style.position = 'relative';
+} else {
+    gsap.registerPlugin(ScrollTrigger);
+
+    // gsap animation
+    let tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: '.intro',
+            start: 'top top',
+            end: 'bottom center',
+            scrub: true,
+            markers: false,
+            pin: true,
+            pinSpacing: false
+        }
+    });
+    
+    tl.to('.intro', {
+        y: -1200
+    });
+}
+
+const queryParams = new URLSearchParams(window.location.search);
+const look = atob(queryParams.get('look')).trim();
+console.log(look);
+
+const popupAnimation = [
+    {transform: "translateY(100vh)"},
+    {transform: "translateY(0)"}
+];
+
+const popdownAnimation = [
+    {transform: "translateY(0)"},
+    {transform: "translateY(100vh)"}
+];
+
+const popupAnimationOptions = {
+    delay: 20,
+    duration: 500, 
+    easing: 'ease-out' 
+};
+
+const dialog = document.querySelector('.popup');
+const lookSection = document.querySelector('#look');
+const headerImage = document.querySelector('#look > .header > .header-image > img');
+const headerTitle = document.querySelector('#look > .header > h1');
+const pairedWithImage = document.querySelector('#look > .header > .paired-with > .image > img');
+const descriptionText = document.querySelector('#look > .description > p');
+const header = document.querySelector('#look > .header');
+const pairedWith = document.querySelector('#look > .header > .paired-with');
+let values;
+
+const closePopup = document.querySelector('#close-icon');
+closePopup.addEventListener('click', () => {
+    lookSection.animate(popdownAnimation, popupAnimationOptions);
+    body.style.height = '';
+    body.style.overflow = '';
+    setTimeout(() => {
+        dialog.close();
+    }, 490);
+    
+});
+
+switch(look) {
+    case 'blue':
+    case 'black':
+    case 'rust':
+    case 'dungaree':
+    case 'beige':
+        console.log(getPageValues(look));
+        values = getPageValues(look);
+        headerImage.src = values['headerImageSource'];
+        headerTitle.innerText = values['headerTitle'];
+        headerTitle.style.color = values['headerTitleColour'];
+        pairedWithImage.src = values['pairedWithImageSource'];
+        descriptionText.innerText = values['descriptionText'];
+        header.style.backgroundColor = values['headerColour'];
+        pairedWith.style.backgroundColor = values['pairedWithBgColour'];
+        closePopup.style.color = values['headerTitleColour'];
+        dialog.showModal();
+        lookSection.animate(popupAnimation, popupAnimationOptions);
+        body.style.height = '100vh';
+        body.style.overflow = 'hidden';
+        if (values['isLast']) {
+            setTimeout(() => {
+                console.log('trigger');
+                const closeHint = document.querySelector('#close-hint');
+                closeHint.style.opacity = 0;
+                closeHint.innerText = 'you can try this now :)';
+                closeHint.animate(
+                    [
+                        {opacity: "0"},
+                        {opacity: "1"}
+                    ],
+                    {
+                        duration: 500, 
+                        easing: 'ease-out' 
+                    }
+                );
+                closeHint.style.opacity = 1;
+            }, 2000);
+        }
+        break;
+    default:
+        console.log('invalid look');
+        break;
+}
+console.log(window.location.href);
+history.replaceState(null, null, "?" + queryParams.toString());
